b1cb7915a18a9f6c98abb39f6bb554753a6b2fb5
palacgui <guillermo.palacio@adidas-group.com>
2017-01-23 18:05:42 +0100

    Cucumber suppoort

---
+++ features/step_definitions/check_version.rb
@@ -0,0 +1,11 @@
+When(/^I send "([^"]*)" request to "([^"]*)"$/) do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the response code should be (\d+)$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the response should match json:$/) do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
